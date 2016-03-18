1339122762aa868395d8e34073daee2471b9a261
Matt Todd <matt.c.todd@gmail.com>
2016-03-18 05:31:27 -0500

    fixed logging issues

--- lib/service.py
+++ lib/service.py
@@ -1,3 +1,4 @@
+import logging
 import thread
 import sys
 
@@ -8,6 +9,8 @@ class Service(object):
     self._data_processor = DataProcessor()
 
   def main(self):
+    logging.basicConfig(filename='/var/log/pi-client/pi-client.log', filemode='w', level=logging.DEBUG)
+
     try:
       thread.start_new_thread(self._data_processor.process, ('process_data', 1))
     except:
